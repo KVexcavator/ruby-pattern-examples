949b578e1526f5cef06df6289ea0fe1221d78d72
Excavator <kvronin@gmail.com>
2023-02-16 23:51:48 +0600

    update folders

--- algorithm_structure/sorting/bubble-sort.rb
+++
@@ -1,1 +0,0 @@
-require_relative "less_more"
--- algorithm_structure/sorting/less_more.rb
+++
@@ -1,8 +0,0 @@
-# вспомогательные функции
-def less(value1, value2)
-  return value1 < value2
-end
-
-def more(value1, value2)
-  return value1 > value2
-end
